e7351a86d421df68df9eca524c87514fd2633c61
Oliver Borchert <oliver.borchert@penflock.com>
2020-03-26 05:34:29 +0100

    Update CI

--- cuckoo.rb
+++ cuckoo.rb
@@ -1,6 +1,6 @@
 class Cuckoo < Formula
     desc "CLI Tool for GitLab CI and Kubernetes Deployments."
-    url "https://circle-artifacts.com/gh/borchero/cuckoo/$CIRCLE_BUILD_NUM/artifacts/0/cuckoo.tar.gz"
+    url "https://circle-artifacts.com/gh/borchero/cuckoo/$CIRCLE_PREVIOUS_BUILD_NUM/artifacts/0/cuckoo.tar.gz"
     sha256 "$ARTIFACT_SHA256"
 
     depends_on "go@1.14" => :build
