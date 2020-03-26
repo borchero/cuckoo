37d89186ec43b5da0efee144c1f25a05877cfed7
Oliver Borchert <oliver.borchert@penflock.com>
2020-03-26 05:56:41 +0100

    Add version to CI

--- cuckoo.rb
+++ cuckoo.rb
@@ -2,6 +2,7 @@ class Cuckoo < Formula
     desc "CLI Tool for GitLab CI and Kubernetes Deployments."
     url "https://circle-artifacts.com/gh/borchero/cuckoo/$CIRCLE_PREVIOUS_BUILD_NUM/artifacts/0/cuckoo.tar.gz"
     sha256 "$ARTIFACT_SHA256"
+    version "$CIRCLE_TAG"
 
     depends_on "go@1.14" => :build
 
